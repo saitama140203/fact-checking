f19d41b3a19c704c11a85b4355eb23c9bb220e58
Saitama <lephu9d@gmail.com>
2025-12-04 00:12:38 +0700

    modify file main

--- backend/main.py
+++ backend/main.py
@@ -123,6 +123,7 @@ app.add_middleware(
         "http://localhost:3000",
         "http://localhost:3001",
         "http://127.0.0.1:3000",
+        "https://fact-checking-iota.vercel.app"
     ],
     allow_credentials=True,
     allow_methods=["*"],
